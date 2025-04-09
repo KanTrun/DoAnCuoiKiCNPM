d1a735f6de90a62cd1dfa6817bf055c47701a57f
Bao Kiet <baokie2004@gmail.com>
2025-04-09 12:23:09 +0700

    Tạo Combobox để cập nhật hiển thị trạng thái đơn hàng

--- DoAnCuoiKy/DoAnCuoiKy/FormOrders.Designer.cs
+++ DoAnCuoiKy/DoAnCuoiKy/FormOrders.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dgvOrders = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.cmbUpdate = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,15 +50,24 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(236, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(268, 39);
+            this.label1.Size = new System.Drawing.Size(259, 38);
             this.label1.TabIndex = 1;
             this.label1.Text = "Danh sách món";
+            // 
+            // cmbUpdate
+            // 
+            this.cmbUpdate.FormattingEnabled = true;
+            this.cmbUpdate.Location = new System.Drawing.Point(90, 314);
+            this.cmbUpdate.Name = "cmbUpdate";
+            this.cmbUpdate.Size = new System.Drawing.Size(121, 24);
+            this.cmbUpdate.TabIndex = 2;
             // 
             // FormOrders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbUpdate);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvOrders);
             this.Name = "FormOrders";
@@ -72,5 +82,6 @@
 
         private System.Windows.Forms.DataGridView dgvOrders;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbUpdate;
     }
 }
