3e8bef995221f8832ad4be750829fba3b761bae8
Bui Ngoc Tram Anh <buingoctramanh58@gmail.com>
2025-04-09 11:54:32 +0700

    Tạo bảng hiển thị dữ liệu khách hàng

---
+++ DoAnCuoiKy/DoAnCuoiKy/customer-lookup-by-phone-or-id.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnCuoiKy
+{
+    public partial class customer_lookup_by_phone_or_id : Form
+    {
+        public customer_lookup_by_phone_or_id()
+        {
+            InitializeComponent();
+        }
+    }
+}
