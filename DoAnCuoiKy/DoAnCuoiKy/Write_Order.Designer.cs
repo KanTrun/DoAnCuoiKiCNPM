b83334349357d863c66d1f4bdbb40684e070aa3d
Huynh trung nhan <huynhnhan2608@gmail.com>
2025-04-09 18:17:45 +0700

    Gxac nhan

--- DoAnCuoiKy/DoAnCuoiKy/Write_Order.Designer.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Write_Order.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Ghi danh sach don";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Chon ban";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Write_Order
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1761, 942);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Write_Order";
             this.Text = "Write_Order";
@@ -56,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
