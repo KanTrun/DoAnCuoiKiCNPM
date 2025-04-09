4750bf2830a6231a1e9bc418bf9d5b03b64cf910
Bui Thi Tra Mi <trami27012004@gmail.com>
2025-04-09 13:03:58 +0700

    Tinh gia tri ma giam gia va cap nhat tong tien

--- DoAnCuoiKy/DoAnCuoiKy/FormDonHang.Designer.cs
+++ DoAnCuoiKy/DoAnCuoiKy/FormDonHang.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -50,11 +52,31 @@
             this.button3.Text = "Ap dung ma giam gia";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Cap nhat tong tien";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(75, 191);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(190, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Tinh gia tri giam gia ";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // FormDonHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Name = "FormDonHang";
@@ -67,5 +89,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
     }
 }
