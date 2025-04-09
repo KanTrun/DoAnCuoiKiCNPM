b299b5d34524431d36b248c0d52a54f152deff9e
Khang Truong <khangtruongminh200804@gmail.com>
2025-04-09 13:13:08 +0700

    Dat lai mon lich su

--- DoAnCuoiKy/DoAnCuoiKy/Check_History_Order.Designer.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Check_History_Order.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,11 +53,21 @@
             this.dataGridView1.Size = new System.Drawing.Size(708, 150);
             this.dataGridView1.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Đặt lại";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Check_History_Order
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "Check_History_Order";
@@ -71,5 +82,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
     }
 }
