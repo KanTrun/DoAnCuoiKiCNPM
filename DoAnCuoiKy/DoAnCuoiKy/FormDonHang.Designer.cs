ef30762f5abfa9ae3228c8162bd2b74460c2bf3c
Bui Thi Tra Mi <trami27012004@gmail.com>
2025-04-09 13:02:27 +0700

    Ap dung ma giam gia

--- DoAnCuoiKy/DoAnCuoiKy/FormDonHang.Designer.cs
+++ DoAnCuoiKy/DoAnCuoiKy/FormDonHang.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -40,11 +41,21 @@
             this.button1.Text = "Kiem tra ma giam gia";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(57, 152);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(248, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Ap dung ma giam gia";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // FormDonHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Name = "FormDonHang";
             this.Text = "FormDonHang";
@@ -55,5 +66,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
     }
 }
