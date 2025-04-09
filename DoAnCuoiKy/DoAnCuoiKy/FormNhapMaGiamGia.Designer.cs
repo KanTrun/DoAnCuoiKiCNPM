514626b610a00d577bb0f31da66647d107491198
Bui Thi Tra Mi <trami27012004@gmail.com>
2025-04-09 13:06:54 +0700

    Cap nhat Form Don Hang hien thi thong tin giam gia

--- DoAnCuoiKy/DoAnCuoiKy/FormNhapMaGiamGia.Designer.cs
+++ DoAnCuoiKy/DoAnCuoiKy/FormNhapMaGiamGia.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labelControl1
@@ -40,11 +41,21 @@
             this.labelControl1.TabIndex = 0;
             this.labelControl1.Text = "Nhap ma giam gia";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cap nhat ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormNhapMaGiamGia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.labelControl1);
             this.Name = "FormNhapMaGiamGia";
             this.Text = "FormNhapMaGiamGia";
@@ -56,5 +67,6 @@
         #endregion
 
         private DevExpress.XtraEditors.LabelControl labelControl1;
+        private System.Windows.Forms.Button button1;
     }
 }
