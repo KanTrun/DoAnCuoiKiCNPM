16a77e649d67f9a6c3fa3b59cc2643ccc462401f
Bui Thi Tra Mi <trami27012004@gmail.com>
2025-04-09 11:13:19 +0700

    Them nut xoa mon vao giao dien don hang

--- DoAnCuoiKy/DoAnCuoiKy/order-management.Designer.cs
+++ DoAnCuoiKy/DoAnCuoiKy/order-management.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -54,18 +55,28 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(290, 354);
+            this.button2.Location = new System.Drawing.Point(299, 355);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(154, 36);
             this.button2.TabIndex = 2;
             this.button2.Text = "Sửa món";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(571, 355);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Xóa món";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // order_management
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -81,5 +92,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
