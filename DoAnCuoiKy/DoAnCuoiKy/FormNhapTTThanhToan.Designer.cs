7439af4fb3f6482fff0993438f5f09c9095e2d43
Bui Thi Tra Mi <trami27012004@gmail.com>
2025-04-09 13:36:01 +0700

    Luu ket qua thanh toan

--- DoAnCuoiKy/DoAnCuoiKy/FormNhapTTThanhToan.Designer.cs
+++ DoAnCuoiKy/DoAnCuoiKy/FormNhapTTThanhToan.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Vui lòng nhập thông tin thanh toán";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Luu ket qua thanh toan";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormNhapTTThanhToan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "FormNhapTTThanhToan";
             this.Text = "FormNhapTTThanhToan";
@@ -56,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
