1347345573d38dc147621c1be40ec95a90a89c63
Bui Ngoc Tram Anh <buingoctramanh58@gmail.com>
2025-04-09 10:44:58 +0700

    Tạo bảng hiển thị danh sách tiêu thụ

--- DoAnCuoiKy/DoAnCuoiKy/Ingredient_consumption_analysis.Designer.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Ingredient_consumption_analysis.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DoAnCuoiKy
 {
-    partial class MainForm
+    partial class Ingredient_consumption_analysis
     {
         /// <summary>
         /// Required designer variable.
@@ -28,49 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(74, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(620, 298);
+            this.dataGridView1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(469, 52);
+            this.label1.Location = new System.Drawing.Point(293, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Quản Lý Quán CaFe";
+            this.label1.Size = new System.Drawing.Size(154, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TỔNG QUAN TIÊU THỤ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(75, 104);
+            this.button1.Location = new System.Drawing.Point(74, 72);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Danh sách";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Lọc";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(75, 158);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(151, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Phân tích tiêu thụ";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // MainForm
+            // Ingredient_consumption_analysis
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1096, 450);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Ingredient_consumption_analysis";
+            this.Text = "Ingredient_consumption_analysis";
+            this.Load += new System.EventHandler(this.Ingredient_consumption_analysis_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,9 +82,8 @@
 
         #endregion
 
+        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
-
