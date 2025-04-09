d9670abc3207ced81ac8f0afaab60ffc85c0d5bb
Huynh trung nhan <huynhnhan2608@gmail.com>
2025-04-09 18:06:14 +0700

    Xuat danh sach nha cung cap

--- DoAnCuoiKy/DoAnCuoiKy/Supplier-History.Designer.cs
+++ DoAnCuoiKy/DoAnCuoiKy/Supplier-History.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -59,11 +60,21 @@
             this.comboBox1.Size = new System.Drawing.Size(121, 39);
             this.comboBox1.TabIndex = 2;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1074, 689);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 87);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Xuat";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Supplier_History
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1990, 1229);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -79,5 +90,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
